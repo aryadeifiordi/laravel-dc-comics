1801aa2fa36b068657e920affdcad74c8b7a2459
aryadeifiordi <arya.93.ar@gmail.com>
2024-05-16 16:00:40 +0200

    route changes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('comics.index');
 });
 
-Route::resource('comics', ComicController::class);
+Route::resource('comics', ComicController::class)->only(['create', 'index', 'show']);
